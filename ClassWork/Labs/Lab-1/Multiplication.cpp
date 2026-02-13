a571ecae48fca954d22ec8245a5f515368a86720
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2026-02-13 20:40:54 +0500

    Corrected the Logic

--- ClassWork/Labs/Lab-1/Multiplication.cpp
+++ ClassWork/Labs/Lab-1/Multiplication.cpp
@@ -3,61 +3,43 @@ using namespace std;
 int main()
 {
     int arr[2][2];
-    int arr1 [2][2];
-
-
-    cout<<"Enter the data of the first 2-D 2x2 array : ";
-
-    for (int i = 0 ; i<2 ;i++)
+    int arr1[2][2];
+    int result[2][2] = {0};
+    cout<<"Enter the data of the first 2x2 matrix:\n";
+    for (int i = 0; i < 2; i++)
     {
-        for (int j=0 ;j<2;j++)
+        for (int j = 0; j < 2; j++)
         {
-            cin>>arr[i][j];
+            cin >> arr[i][j];
+        }
+    }
+    cout<<"Enter the data of the second 2x2 matrix:\n";
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> arr1[i][j];
         }
     }
 
-    cout<<"Enter the data of the Second 2-D 2x2 array : ";
-
-    for (int i = 0 ; i<2 ;i++)
+    for (int i = 0; i < 2; i++)
     {
-        for (int j=0 ;j<2;j++)
+        for (int j = 0; j < 2; j++)
         {
-            cin>>arr1[i][j];
+            for (int k = 0; k < 2; k++)
+            {
+                result[i][j] += arr[i][k] * arr1[k][j];
+            }
         }
     }
-
-    cout<<"Data that you entered for first array is  : "<<endl;
-    for (int i = 0 ; i<2 ;i++)
+    cout << "Product Matrix:\n";
+    for (int i = 0; i < 2; i++)
     {
-        for (int j=0 ;j<2;j++)
+        for (int j = 0; j < 2; j++)
         {
-            cout<<arr[i][j]<<" ";
+            cout << result[i][j] << " ";
         }
-        cout<<"\n";
+        cout << endl;
     }
-
-    cout<<"Data that you entered for second array is  : "<<endl;
-    for (int i = 0 ; i<2 ;i++)
-    {
-        for (int j=0 ;j<2;j++)
-        {
-            cout<<arr1[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-
-
-    cout<<"The product matrix of the first and second array is : \n";
-    for (int i = 0 ; i<2 ;i++)
-    {
-        for (int j=0 ;j<2;j++)
-        {
-            cout<<arr[i][j]*arr1[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-
-
-
-
+    return 0;
 }
